ca243fa374b927576ff4a812b9033a5b3cb06f67
Arvind Shelke <ucdonor5@gmail.com>
2021-08-15 14:08:44 +0530

    done

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+$(".btn").click(()=>{
+    function randomNum(){
+        return Math.round(Math.random()*(hex.length-1));
+    }
+    var hexStr="#";
+    for(var i=0; i<=5; i++){
+        console.log(hexStr);
+        hexStr+=""+hex[randomNum()];
+    }
+    $('body').css("background-color", hexStr);
+    $(".color").css('color', hexStr);
+    $(".color").text(hexStr);
+});
