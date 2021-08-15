516b9fb8d45a8fc2dfa3f0dcd1bc5e17dfd371e2
Arvind Shelke <ucdonor5@gmail.com>
2021-08-15 13:48:26 +0530

    20:08

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,11 @@
+// 
+/// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
+
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+$(".btn").click(()=>{
+    const randomNum=Math.round(Math.random()*(colors.length-1));
+    $('body').css("background-color", colors[randomNum]);
+    $(".color").css('color', colors[randomNum]);
+    $(".color").text(colors[randomNum]);
+});
