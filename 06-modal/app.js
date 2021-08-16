009200919567d1c7f7b6904ba59318ce61b11025
Arvind Shelke <ucdonor5@gmail.com>
2021-08-16 11:09:26 +0530

    1:43:09

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -2,3 +2,11 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+$(".modal-btn").click(()=>{
+    $(".modal-overlay").addClass("open-modal")
+});
+
+$(".close-btn").click(()=>{
+    $(".modal-overlay").removeClass("open-modal")
+});
