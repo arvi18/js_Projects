b253f5c4598363c16f5ceec60551392fad45501d
Arvind Shelke <ucdonor5@gmail.com>
2021-08-16 22:19:47 +0530

    setup-09

---
+++ 09-video/app.js
@@ -0,0 +1,4 @@
+/// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
