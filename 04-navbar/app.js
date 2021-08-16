4f65e3429dc4b73f30f82ab66f3638a5afb6f55a
Arvind Shelke <ucdonor5@gmail.com>
2021-08-16 10:36:20 +0530

    1:26:30

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -1,5 +1,9 @@
+/// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
 // classList - shows/gets all classes
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
 // toggle - toggles class
+$(".nav-toggle").click(()=>{
+    $(".links").toggleClass("show-links");
+});
