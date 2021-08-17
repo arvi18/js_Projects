6cf3dbda0d876f574851ebbd5ada63c8f1c2c092
Arvind Shelke <ucdonor5@gmail.com>
2021-08-17 17:58:23 +0530

    2:49:52

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -98,7 +98,7 @@ let displayMenu=menu.map(function(item){
    <div class="item-info">
      <header>
        <h4>${item.title}</h4>
-     <h4 class="price">${item.price}</h4>
+     <h4 class="price">${item.price}$</h4>
      </header>
      <p class="item-text">${item.desc}</p>
    </div>
