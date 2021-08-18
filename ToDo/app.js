7c8aaa99a98c428a9bda47504ef8d5ff21dcd86d
Arvind Shelke <ucdonor5@gmail.com>
2021-08-18 22:31:31 +0530

    done

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,6 +1,5 @@
 /// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
 
-
 $(".todo-button").click((e)=>{
     e.preventDefault();
     $(`<div class="proc"><li class="todo-item">${$(".todo-input").val()}</li><div class="btns"><button id="check"><i class="fas fa-check"></i></button><button id="trash"><i class="fas fa-trash"></i></button></div></div>`).prependTo(".todo");
@@ -12,4 +11,3 @@ $(".todo-button").click((e)=>{
         $(this).closest('.proc').remove();
     });
 });
-
