e7eb60e98ee55147d94cbd9461e4e77205eb2e9e
Arvind Shelke <ucdonor5@gmail.com>
2021-08-18 21:56:42 +0530

    check and trash done

--- ToDo/app.js
+++ ToDo/app.js
@@ -5,11 +5,11 @@ $(".todo-button").click((e)=>{
     e.preventDefault();
     $(`<div class="proc"><li class="todo-item">${$(".todo-input").val()}</li><div class="btns"><button id="check"><i class="fas fa-check"></i></button><button id="trash"><i class="fas fa-trash"></i></button></div></div>`).prependTo(".todo");
     $(".todo-input").val("")
-    $(".fa-check").click(function(){
-        
-        $(this).parent().parent().parent().toggleClass("crossed")
+    $("#check").click(function(){    
+        console.log($(this).closest('.proc').toggleClass("crossed"))
     });
-    $(".fa-trash").click(function(){
-        $(this).parent().parent().parent().remove()
+    $("#trash").click(function(){
+        $(this).closest('.proc').remove();
     });
 });
+
