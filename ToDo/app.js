0ffab9f445fb9820000b2ca1f6312abb29948a06
Arvind Shelke <ucdonor5@gmail.com>
2021-08-18 21:17:01 +0530

    failed

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,7 +1,15 @@
 /// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
 
+
 $(".todo-button").click((e)=>{
     e.preventDefault();
-    $(`<div class="proc"><li class="todo-item">hey</li><button class="complete-button"><i class="fas fa-check"></i></button><button class="complete-button"><i class="fas fa-trash"></i></button></div>`).appendTo(".todo");
+    $(`<div class="proc"><li class="todo-item">${$(".todo-input").val()}</li><div class="btns"><button id="check"><i class="fas fa-check"></i></button><button id="trash"><i class="fas fa-trash"></i></button></div></div>`).prependTo(".todo");
+    $(".todo-input").val("")
+    $(".fa-check").click(function(){
+        
+        $(this).parent().parent().parent().toggleClass("crossed")
+    });
+    $(".fa-trash").click(function(){
+        $(this).parent().parent().parent().remove()
+    });
 });
-
