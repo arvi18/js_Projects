b6c31ae0974c0fba2b1862f85d0db373a37834bf
Arvind Shelke <ucdonor5@gmail.com>
2021-08-21 08:20:24 +0530

    pause and play done

--- 09-video/app.js
+++ 09-video/app.js
@@ -2,3 +2,10 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const switchBtn=$(".switch-btn")
+switchBtn.click(()=>{
+    switchBtn.hasClass("slide") ? (switchBtn.removeClass("slide"),
+    document.querySelector(".video-container").pause())
+    :(switchBtn.addClass("slide"),document.querySelector(".video-container").play())
+})
