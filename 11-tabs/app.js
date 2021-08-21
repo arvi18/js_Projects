d56ad1b57207085ca11f0ed3940dea6543692516
Arvind Shelke <ucdonor5@gmail.com>
2021-08-21 16:19:20 +0530

    done 4:56:13

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -1,1 +1,8 @@
 /// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
+
+$(".tab-btn").click(function(){
+    $(".tab-btn").removeClass("active");
+    $(".content").removeClass("active");
+    $(this).addClass("active");
+    $(".content#"+this.id).addClass("active");
+});
