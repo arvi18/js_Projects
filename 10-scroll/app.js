57d0d09dcecd076146fc8c0dcde8ab9e1210b01c
Arvind Shelke <ucdonor5@gmail.com>
2021-08-21 08:24:44 +0530

    setup-scroll

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -1,3 +1,4 @@
+/// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
 // Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
 // pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
 // slice extracts a section of a string without modifying original string
