17c13c03c8f27f8ae131e8a5d3a60e8b011e7279
Arvind Shelke <ucdonor5@gmail.com>
2021-08-25 20:09:41 +0530

    completed-uncompleted filter done

--- ToDo/app.js
+++ ToDo/app.js
@@ -3,12 +3,20 @@
 $(".todo-button").click((e)=>{
     e.preventDefault();
     $(`<div class="proc"><li class="todo-item">${$(".todo-input").val()}</li><div class="btns"><button id="check"><i class="fas fa-check"></i></button><button id="trash"><i class="fas fa-trash"></i></button></div></div>`).prependTo(".todo");
-    $(".todo-input").val("")
+    $(".todo-input").val("");
     $("#check").click(function(){    
-        console.log($(this).closest('.proc').toggleClass("crossed"))
+        $(this).closest('.proc').toggleClass("crossed");
     });
     $("#trash").click(function(){
         $(this).closest('.proc').fadeOut(600);
         setTimeout(()=>$(this).closest('.proc').remove(), 700)
     });
+    
 });
+$("#filter-todo").click(function(){
+    const todos=$("#filter-todo  > option:selected");
+    todos.val()==="all" ? $(".proc").removeClass("trashed")
+    : todos.val()==="completed" ? ($(".proc").removeClass("trashed"), $(".proc").not(".crossed").addClass("trashed") )
+    : todos.val()==="uncompleted" ? ($(".proc").removeClass("trashed"), $(".crossed").addClass("trashed"))  : console.log('done') 
+
+})
