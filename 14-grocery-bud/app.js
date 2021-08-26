b944a15e01767ab8ef3f740a4ca105fcfc512876
Arvind Shelke <ucdonor5@gmail.com>
2021-08-26 23:21:28 +0530

    setup 14-grocery 6:19:10

---
+++ 14-grocery-bud/app.js
@@ -0,0 +1,12 @@
+/// <reference path="D:\codin\WebD\Js\Node.js\typings\globals\jquery\index.d.ts" />
+// ****** SELECT ITEMS **********
+
+// edit option
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
